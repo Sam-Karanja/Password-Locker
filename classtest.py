e9a4db22151dabd1a9fbca922be11a213560c001
Moringa School <karanjasam210@gmail.com>
2022-04-26 08:08:19 +0300

    impor t the unittest module

--- classtest.py
+++ classtest.py
@@ -1,3 +1,11 @@
 import unittest
 from main import Users
 from main import Credentials
+
+
+class TestClass(unittest.TestCase):
+    """a test class that defines test cases for the User class"""
+
+def setUp(self):
+   """method that runs befor each individual test method runss"""
+    self.new_user = Users('Ogaye Mike','yuvicytr')
