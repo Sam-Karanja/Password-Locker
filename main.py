bee55e6b892b218b9b6ed48007bc595f6635381f
Moringa School <karanjasam210@gmail.com>
2022-04-26 05:53:13 +0300

    add main.py file and create the user class and definitions

---
+++ main.py
@@ -0,0 +1,20 @@
+class Users:
+
+   users=[]
+   def __init__(self,username,password):
+
+        self.username=username
+        self.password=password
+
+   def adduser (self,user):
+       if user not in  self.users:
+        self.users.append(user)
+
+
+user= input('Enter your user name:')
+password=input('Enter your password:')
+user1=Users(user,password)
+print(user1.password)
+print (user1.username)
+user1.adduser(user)
+print(user1.users) 
