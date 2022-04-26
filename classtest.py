3e4fb176197a3eea696741b55f6651ddbaba868d
Moringa School <karanjasam210@gmail.com>
2022-04-26 08:21:24 +0300

    initialize the user and credential class for testing

--- classtest.py
+++ classtest.py
@@ -8,7 +8,7 @@ class TestClass(unittest.TestCase):
 
 def setUp(self):
    """method that runs befor each individual test method runss"""
-    self.new_user = Users('SamKaranja','yuvicytr')
+    self.new_user = Users('SamKaranja','skmurigi')
 
  def test_init(self):
         """
@@ -16,3 +16,27 @@ def setUp(self):
         """
         self.assertEqual(self.new_user.username,'SamKaranja')
         self.assertEqual(self.new_user.password,'yuvicytr')
+
+       def test_save_user(self):
+        """
+        test case to test if a new user instance has been saved into the User list
+        """
+        self.new_user.adduser()
+        self.assertEqual(len(Users.user_list),1)
+
+class TestCreds(unittest.TestCase):
+    """
+    A test class that defines test cases for credentials class
+    """ 
+    def setUp(self):
+        """
+        Method that runs before each individual credentials test methods run.
+        """
+        self.new_credential = Credentials('facebook','Sam Karanja','beaut56')
+    def test_init(self):
+        """
+        Test case to check if a new Credentials instance has been initialized correctly
+        """
+        self.assertEqual(self.new_credential.account,'facebook')
+        self.assertEqual(self.new_credential.userName,'Sam Karanja')
+        self.assertEqual(self.new_credential.password,'beaut56')
